64502a655baf79ba093f2c9d3c0dc2ff99e02d0d
Chelsea Church <chelsea.ae.church@gmail.com>
2021-07-30 15:48:18 -0400

    updated key

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const imageContainer = document.getElementById('image-container')
 const loader = document.getElementById('loader')
 
+console.log('updated')
 let ready = false
 let imagesLoaded = 0
 let totalImages = 0
@@ -8,8 +9,9 @@ let photosArray = [];
 
 //Unsplash API
 const count = 15
-const apiKey =  secrets.MY_KEY || config.MY_KEY
+const apiKey =  secrets.MY_KEY 
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
+
 
 //Check if all images were loaded
  function imageLoaded() {
